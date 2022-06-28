54c09125bab94e50411a4b0010e93dc8593ca8f3
Gustavo Condezo Camayo <gustavocondezocamayo@gmail.com>
2022-06-28 00:26:28 -0500

    Micros

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,9 +5,13 @@ const app = express()
 
 app.use(express.json())
 
+const events = []
+
 app.post('/events', (req,res)=>{
     try{
         const event = req.body
+
+        events.push(event)
 
         void axios.post('http://localhost:4000/events',event)
         void axios.post('http://localhost:4001/events',event)
@@ -20,6 +24,10 @@ app.post('/events', (req,res)=>{
         res.send({status:'OK'})
 
     }
+})
+
+app.get('/events',(req,res)=>{
+    res.send(events)
 })
 
 app.listen(4005,()=>{
--- query/index.js
+++ query/index.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-
+const axios =require('axios')
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const posts = {}
 
-app.get('/posts', (req,res)=>{
-    res.send(posts)
-})
-
-app.post('/events',(req,res)=>{
-    const {type, data} = req.body
-
+const handleEvent = (type,data)=>{
     if(type==='PostCreated'){
         const {id, title} = data
         posts[id] = {id,title, comments:[]}
@@ -33,10 +27,31 @@ app.post('/events',(req,res)=>{
         comment.content = content
 
     }
+}
+
+app.get('/posts', (req,res)=>{
+    res.send(posts)
+})
+
+app.post('/events',(req,res)=>{
+    const {type, data} = req.body
+
+    handleEvent(type,data)
     console.log(posts)
     res.send({})
 })
 
-app.listen(4002, ()=>{
-    console.log('Listening on 4002')
+app.listen(4002, async ()=>{
+    try{
+        console.log('Listening on 4002')
+        const res = await axios.get('http://localhost:4005/events')
+
+        for (let event of res.data){
+            console.log('Processing event:', event.type)
+
+            handleEvent(event.type, event.data)
+        }
+    }catch (e) {
+        console.error(e.message)
+    }
 })
